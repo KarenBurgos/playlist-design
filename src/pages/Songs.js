940abfef8572a4ecdc9b09a3f61d5bcf6f18b45d
KarenBurgos <00043820@uca.edu.sv>
2023-06-25 01:42:44 -0600

    Add getSongs services

--- src/pages/Songs.js
+++ src/pages/Songs.js
@@ -2,52 +2,59 @@ import Menu from "../components/menu";
 import wave1 from '../assets/wave1-home.png';
 import wave2 from '../assets/wave2-home.png';
 import Song from '../components/Song';
+import { getSongs } from "../services/SongxPlaylist";
+import { useEffect, useState } from "react";
 
-function Songs(){
-    const calculateRemainingHeight = () => {
-        const headerHeight = 200; // Ajusta esto según la altura del encabezado
-        const footerHeight = 200; // Ajusta esto según la altura del pie de página
-        const windowHeight = window.innerHeight;
-        return windowHeight - headerHeight - footerHeight;
-      };
+function Songs() {
+  const [songs, setSongs] = useState([]);
 
-    return(
-        <div class="h-screen relative from-purple-gradient to-pink-gradient bg-gradient-to-b">
-           <Menu/> 
+  const calculateRemainingHeight = () => {
+    const headerHeight = 200; // Ajusta esto según la altura del encabezado
+    const footerHeight = 200; // Ajusta esto según la altura del pie de página
+    const windowHeight = window.innerHeight;
+    return windowHeight - headerHeight - footerHeight;
+  };
 
-            <div className="py-10 px-28 relative z-10">
-                <div className="grid grid-cols-2 w-1/4">
-                    <div>
-                        <h1 className="text-lg font-semibold">Nombre playlist</h1> {/* //TODO: Mostrar nombre de la playlist */}
-                        <h2 className="text-white">Duracion: 3:15:04</h2> {/* //TODO: Mostrar duracion de la playlist */}
-                    </div>
-                    <button className="bg-purple-dark text-white px-8 py-2 rounded">Agregar canción</button>
-                </div>
-                    <Song title="Song 1" duration="5:00"/>
-                <div className="mt-16 overflow-y-auto" style={{ maxHeight: `${calculateRemainingHeight()}px` }}>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
-                    <Song title="Song 1" duration="5:00"/>
+  useEffect(() => {
+    const fetchSongs = async () => {
+      try {
+        const data = await getSongs("prueba3");
+        setSongs(data.songs);
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    };
 
+    fetchSongs();
+  }, []);
 
-                </div>
-            </div>
-            
+  return (
+    <div className="h-screen relative from-purple-gradient to-pink-gradient bg-gradient-to-b">
+      <Menu />
 
-
-           <img src={wave1} className="absolute -z-0 bottom-0 w-full h-1/3" />
-            <img src={wave2} className="absolute -z-0 bottom-0 w-full h-1/3" />
+      <div className="py-10 px-28 relative z-10">
+        <div className="grid grid-cols-2 w-1/4">
+          <div>
+            <h1 className="text-lg font-semibold">Nombre playlist</h1>
+            {/* //TODO: Mostrar nombre de la playlist */}
+            <h2 className="text-white">Duracion: 3:15:04</h2>
+            {/* //TODO: Mostrar duracion de la playlist */}
+          </div>
+          <button className="bg-purple-dark text-white px-8 py-2 rounded">
+            Agregar canción
+          </button>
         </div>
-    );
+        <div className="mt-16 overflow-y-auto" style={{ maxHeight: `${calculateRemainingHeight()}px` }}>
+        {songs.map((song) => (
+            <Song title={song}/>
+          ))}
+        </div>
+      </div>
+
+      <img src={wave1} className="absolute -z-0 bottom-0 w-full h-1/3" />
+      <img src={wave2} className="absolute -z-0 bottom-0 w-full h-1/3" />
+    </div>
+  );
 }
 
 export default Songs;
---
+++ src/services/SongxPlaylist.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+const baseURL = "http://localhost:8080/songxplaylist/";
+const token = localStorage.getItem("token");
+
+const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const addSong = async (title, description) => {
+    const data = {
+      title: title,
+      description: description
+    };
+  
+    try {
+      const response = await axios.post(baseURL + '/', data, config);
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log('Error:', error);
+      throw error;
+    }
+  };
+  
+
+const getSongs = (titlePlaylist) => {
+    return axios.get(baseURL + titlePlaylist +'/songs', config)
+      .then((response) => {
+        console.log(response.data)
+        return response.data
+      })
+      .catch((error) => {
+        toast.error("Error")
+        if (error.response) {
+          console.log('Error:', error.response.data);
+        } else {
+          console.log('Error:', error.message);
+        }
+        throw error; // Propagar el error para manejarlo en el componente
+      });
+  };
+
+export { addSong, getSongs };
