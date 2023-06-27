2b0ef2289e88edab50963619ba87e554fd31f3b8
KarenBurgos <00043820@uca.edu.sv>
2023-06-27 11:38:38 -0600

    Add function add song to playlist

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,16 +1,45 @@
 import React, { useState, useEffect } from "react";
 import { RiPlayList2Line } from "react-icons/ri";
 import PlaylistInfo from "./PlaylistInfo";
-
-import { getPlaylistsWithSongs } from "../services/SongXPlaylist";
+import { PiMusicNotesPlusBold } from "react-icons/pi";
+import { getPlaylistsWithSongs, addSongToPlaylist } from "../services/SongXPlaylist";
+import AddSongToPlaylistForm from "./AddSongToPlaylistForm";
 
 function Playlist({ id, title, description, onClick }) {
   const [showInfo, setShowInfo] = useState(false); // Estado para mostrar/ocultar info
   const [token, setToken] = useState(localStorage.getItem("token")); // Estado para almacenar el token
   const [totalDuration, setTotalDuration] = useState(false);
+  const [showIcons, setShowIcons] = useState(false);
+  const [showAddSongForm, setShowAddSongForm] = useState(false);
+
+  function onMouseOver() {
+    setShowIcons(true);
+  }
+  function onMouseLeave() {
+    setShowIcons(false);
+  }
+
+  function handleShowForm() {
+    setShowAddSongForm(true);
+  }
+
+  const handleCloseForm = () => {
+    setShowAddSongForm(false);
+  };
 
   const handleInfo = () => {
     setShowInfo(!showInfo);
+  };
+
+  const handleSubmitForm = async (data) => {
+    try {
+      const response = await addSongToPlaylist(id, data.code, token);
+      console.log(response);
+      setShowAddSongForm(false);
+     
+    } catch (error) {
+      console.log("Error:", error);
+    }
   };
 
   useEffect(() => {
@@ -28,18 +57,34 @@ function Playlist({ id, title, description, onClick }) {
   return (
     <div className="bg-white rounded-lg p-4 mt-3">
       <div
-        className="bg-white rounded-lg p-4 flex items-center"
+        className="bg-white rounded-lg p-4 flex items-center justify-between"
         onClick={handleInfo}
+        onMouseOver={onMouseOver}
+        onMouseOut={onMouseLeave}
       >
-        <RiPlayList2Line className="text-blue-500 text-2xl mr-2" />
+        <div className="flex items-center">
+        <RiPlayList2Line className="text-blue-500 text-2xl mr-5" />
         <div key={id}>
           <h2 className="text-lg font-bold">{title}</h2>
           <p className="text-gray-500">{description}</p>
           <p className="text-gray-500">Duración total: {totalDuration}</p>
+
         </div>
+        </div>
+        {showIcons && (
+          <div className="flex">
+            <PiMusicNotesPlusBold
+              className="text-blue-500 text-2xl mr-2 cursor-pointer"
+              onClick={handleShowForm}
+            />
+          </div>
+        )}
+        {showAddSongForm && (
+          <AddSongToPlaylistForm onClose={handleCloseForm} onSubmit={handleSubmitForm}/>
+        )}
       </div>
       {showInfo && ( // Mostrar el formulario si showForm es verdadero
-        <PlaylistInfo id={id}/>
+        <PlaylistInfo id={id} />
       )}
     </div>
   );
